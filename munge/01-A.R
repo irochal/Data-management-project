f0d2a88b4eba2500946a16dc2300affe6415cf1f
Harry Syllantavos <harrysyllantavos@macbook-air.home>
2021-11-20 13:30:33 +0000

    Data preprocessing 1

--- munge/01-A.R
+++ munge/01-A.R
@@ -75,4 +75,56 @@ library(tidyverse)
 
 all_run_activity_step = do.call(rbind, list(ndf1, ndf2, ndf3, ndf4, ndf5, ndf6, ndf7))
 class(all_run_activity_step)
+all_run_activity_step
+
+
+
+cyber_security_3_video_stats <- 
+  read_csv("data/FutureLearn MOOC Dataset (1)/cyber-security-3_video-stats.csv")
+
+cyber_security_4_video_stats <- 
+  read_csv("data/FutureLearn MOOC Dataset (1)/cyber-security-4_video-stats.csv")
+
+cyber_security_5_video_stats <- 
+  read_csv("data/FutureLearn MOOC Dataset (1)/cyber-security-5_video-stats.csv")
+
+cyber_security_6_video_stats <- 
+  read_csv("data/FutureLearn MOOC Dataset (1)/cyber-security-6_video-stats.csv")
+
+cyber_security_7_video_stats <- 
+  read_csv("data/FutureLearn MOOC Dataset (1)/cyber-security-7_video-stats.csv")
+
+
+
+video_df3 = data.frame(cyber_security_3_video_stats$step_position, cyber_security_3_video_stats[,9:15]) 
+colnames(video_df3) = c("Step position",5,10,25,50,75,95,100)
+all_video_3 = pivot_longer(video_df3, cols = !starts_with("Step"))
+view(all_video_3)
+
+
+
+plot(cyber_security_3_video_stats$viewed_onehundred_percent, cyber_security_3_video_stats$video_duration,
+     xlab = "% of people who watched 100% of the video", ylab = "Video duration in minutes",
+     main = "Plot of video duration vs engagement")
+abline(lm(cyber_security_3_video_stats$video_duration ~ cyber_security_3_video_stats$viewed_onehundred_percent),
+       lty = 2 , col = "red")
+
+cor(cyber_security_3_video_stats$viewed_onehundred_percent, cyber_security_3_video_stats$video_duration)
+
+# We observe that there is some negative correlation between engagement and duration. So in general
+# when the duration is higher, less people tend to watch 100% of the videos. 
+
+
+duration = rep(cyber_security_3_video_stats$video_duration , times = 7)
+
+ndf3_video = data.frame(engagement = cyber_security_3_video_stats$viewed_onehundred_percent,
+                        duration = cyber_security_3_video_stats$video_duration)
+
+
+ndf3_video_week1 = all_video_3[1:35,]
+ndf3_video_week2 = all_video_3[36:63,]
+ndf3_video_week3 = all_video_3[64:91,]
+
+
+
 
